7a0d3577d624dd03709812d50a2fea9470e05bbc
Florian Hübner <code@hub.lol>
2023-11-18 18:53:50 +0100

    add doggo

---
+++ doggo.js
@@ -0,0 +1,21 @@
+const sounds = [
+    'sounds/headshot.mp3', 'sounds/headshot.mp3',
+    'sounds/grenade-out.mp3',
+    'sounds/flashbang.mp3',
+    'sounds/molotov.mp3',
+    'sounds/incendiary.mp3',
+    'sounds/move-it.mp3',
+    'sounds/no-my-friend.mp3', 'sounds/no-my-friend.mp3'
+]
+
+function playSound() {
+    new Audio(sounds[Math.floor(Math.random() * sounds.length)]).play();
+}
+
+let doggo = document.createElement('img')
+doggo.src = 'doggo.png'
+doggo.id = 'doggo'
+doggo.alt = 'dog with ak47'
+doggo.onclick = playSound
+
+document.body.appendChild(doggo)
