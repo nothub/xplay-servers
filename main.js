0d6124868fb9af09216a593507f05a4b25b7686d
Florian Hübner <code@hub.lol>
2023-11-30 00:01:25 +0100

    upstream api changes

--- main.js
+++ main.js
@@ -15,73 +15,87 @@ function addHeader(table) {
     }
 }
 
-function addServer(table, server) {
-    let row = table.insertRow()
-    let cell = row.insertCell()
-    let link = document.createElement('a')
-    link.innerText = `${server.IP}:${server.Port}`
-    link.href = `steam://connect/${server.IP}:${server.Port}`
-    cell.appendChild(link)
-    for (let text of [
-        server.CurrentMap,
-        server.Online,
-        server.TotalSlots,
-        server.AverageFaceItLvl,
-        server.CountryCode,
-        server.TickRate
-    ]) {
-        let cell = row.insertCell()
-        let p = document.createElement('p')
-        p.innerText = text
-        cell.appendChild(p)
-    }
+function newModeTable(slug) {
+    let s = document.createElement('section')
+    document.getElementById('main').appendChild(s)
+
+    let h = document.createElement('h2')
+    h.innerText = slug
+    s.appendChild(h)
+
+    let n = document.createElement('a')
+    n.innerText = slug
+    n.href = `#${slug}`
+    n.appendChild(document.createElement('br'))
+    document.getElementById('nav').appendChild(n)
+
+    let t = document.createElement('table')
+    t.className = 'sortable'
+    t.id = slug
+    addHeader(t)
+    s.appendChild(t)
+
+    return t
 }
 
 (async function () {
-    let modes = []
+
+    let modeData = []
     await fetch('https://xplay.gg/api/play/getCurrentOnlineStatus', {
         method: 'GET', headers: {
             'Accept': 'application/json'
         },
     })
         .then(res => res.json())
-        .then(data => modes = data.GameModes.cs2)
+        .then(data => modeData = data.GameModes)
         .catch(err => console.log(`Failed to fetch modes; ${err}`))
 
-    for (const mode of modes) {
-        let s = document.createElement('section')
-        document.getElementById('main').appendChild(s)
-
-        let h = document.createElement('h2')
-        h.innerText = mode.Slug
-        s.appendChild(h)
-
-        let n = document.createElement('a')
-        n.innerText = mode.Slug
-        n.href = `#${mode.Slug}`
-        n.appendChild(document.createElement('br'))
-        document.getElementById('nav').appendChild(n)
-
-        let t = document.createElement('table')
-        t.className = 'sortable'
-        t.id = mode.Slug
-        addHeader(t);
-        s.appendChild(t)
-
-        let servers = []
-        await fetch('https://xplay.gg/api/play/getServers', {
-            method: 'POST', body: JSON.stringify({modeID: mode.GameModeID, showFullServers: true}), headers: {
-                'Accept': 'application/json', 'Content-Type': 'application/json'
-            },
-        })
-            .then(res => res.json())
-            .then(data => servers = data)
-            .catch(err => console.log(`Failed to fetch servers for ${mode.Slug}; ${err}`))
-
-        for (let server of servers) {
-            addServer(t, server);
-        }
-
-        sorttable.makeSortable(t);
+    const modes = new Map();
+    for (const mode of modeData) {
+        modes.set(mode.GameModeID, mode)
     }
+
+    for (const mode of modes.values()) {
+        mode.Table = newModeTable(mode.Slug)
+        mode.Cmds = []
+    }
+
+    let allData = []
+    await fetch('https://xplay.gg/api/play/getAllServers', {
+        method: 'GET', headers: {
+            'Accept': 'application/json'
+        },
+    })
+        .then(res => res.json())
+        .then(data => allData = data)
+        .catch(err => console.log(`Failed to fetch servers and commands; ${err}`))
+
+    for (let server of allData.serversList) {
+        let row = modes.get(server.GameModeID).Table.insertRow()
+        let cell = row.insertCell()
+        let link = document.createElement('a')
+        link.innerText = `${server.IP}:${server.Port}`
+        link.href = `steam://connect/${server.IP}:${server.Port}`
+        cell.appendChild(link)
+        for (let text of [server.CurrentMap, server.Online, server.TotalSlots, server.AverageFaceItLvl, server.CountryCode, Math.round(server.TickRate)]) {
+            let cell = row.insertCell()
+            let p = document.createElement('p')
+            p.innerText = text
+            cell.appendChild(p)
+        }
+    }
+
+    for (let cmd of allData.commandsList) {
+        if (cmd.CS2 === 1) {
+            modes.get(cmd.GameModeID).Cmds.push(cmd)
+        }
+    }
+
+    for (const mode of modes.values()) {
+        sorttable.makeSortable(mode.Table);
+        for (const cmd of mode.Cmds) {
+            console.log(cmd.Command, "-", cmd.Description)
+        }
+    }
+
 })()
